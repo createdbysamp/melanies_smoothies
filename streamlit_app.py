a7aadb811a730bf3a938d609dc81a87112a25edb
Sampson Ma <createdbysamp@gmail.com>
2026-06-10 16:02:52 -0500

    Update Streamlit app for Snowflake connection
    Refactor Streamlit app to use new connection syntax and remove commented code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 # Import python packages
 import streamlit as st
-# import os
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,16 +8,12 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
-
-# option = st.selectbox (
-#     'What is your favorite fruit?',
-#     ('Banana', 'Strawberries', 'Peaches'))
-# st.write('Your favorite fruit is:', option)
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+# OG streamlit syntax
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
