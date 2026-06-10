0e440b1c042b4be191a1afd24cd427b8d7b7045b
Sampson Ma <createdbysamp@gmail.com>
2026-06-10 17:00:29 -0500

    Display search value for selected fruits
    Added functionality to display search value for chosen fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        # add search_on value
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         # display smoothiefroot api nutrition info
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
